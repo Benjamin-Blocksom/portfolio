a10c9b902e9614dd3bf13b75194d21298dcdcecc
AbraCadabstrax <benjamin.blocksom@gmail.com>
2019-12-19 02:42:56 -0500

    Finalize scripts for form. Add sticky nav bar. Still pending: about, wave decoration, articles, footer.

---
+++ new-site/handler.js
@@ -0,0 +1,56 @@
+const serverless = require("serverless-http");
+const AWS = require("aws-sdk");
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+if (!AWS.config.region) {
+  AWS.config.update({
+    region: "us-east-1"
+  });
+}
+
+const ses = new AWS.SES();
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.post("/", (req, res) => {
+  const name = req.body.name;
+  const email = req.body.email;
+  const message = req.body.message;
+
+  const emailParams = {
+    Source: "benjamin.blocksom@gmail.com",
+    Destination: {
+      ToAddresses: ["benjamin.blocksom@gmail.com"] // Your verfied Email
+    },
+    ReplyToAddresses: [req.body.email],
+    Message: {
+      Body: {
+        Text: {
+          Charset: "UTF-8",
+          Data: `${message}  from  ${req.body.email}`
+        }
+      },
+      Subject: {
+        Charset: "UTF-8",
+        Data: "You Received a Message from www.benblocksom.com"
+      }
+    }
+  };
+
+  ses.sendEmail(emailParams, (err, data) => {
+    if (err) {
+      res.status(402).send(`${err} ${err.stack}`);
+    }
+    if (data) {
+      res.send(data);
+    }
+  });
+});
+
+module.exports.form = serverless(app);
